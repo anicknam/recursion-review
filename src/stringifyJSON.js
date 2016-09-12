63ddf4584ef55841487db6a0fa183f415b356c86
anicknam <a.nicknam@gmail.com>
2016-09-12 12:01:42 -0700

    Changed spec and implemented stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //numbers, null, boolean
+  if (typeof obj === "number" || obj === null || typeof obj === "boolean") {
+    return "" + obj;
+  }
+  //array
+  if (Array.isArray(obj)) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return "[" + arr.join(',') + "]"; 
+  }
+  //object
+  if (typeof obj === "object") {
+    var arr = [];
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === "function") {
+        continue;
+      }
+      arr.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+    }
+    return "{" + arr.join(',') + "}";
+  }
+  //string
+  if (typeof obj === "string") {
+    return '"' + obj + '"';
+  }
+  
+  //functions, undefined
+  if (typeof obj === "function" || obj === undefined) {
+    return ;
+  }
 };
