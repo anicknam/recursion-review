6cf1966502846c2a3796665a16de143caea9a9f3
anicknam <a.nicknam@gmail.com>
2016-09-12 19:26:36 -0700

    Implemented mutual recursive functions: helper functions to parse different data types, function to find data type

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,14 +3,83 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  json = json.substring(1,json.length-1);
+  //json = json.substring(1,json.length-1);
+  json = json.replace(/\r\n|\t|\n\r|\n/g,"");
+
   // array
-  if ( json[0]==="[" ) {
+  function parseArray(array){
+    array = array.substring(1,array.length-1);
     var arr = [];
-    var i = 0;
-    while (json[i]!== "]") {
-      arr.push(json[i]);
-      i++;
+    var elements = array.split(",");
+
+    for (var i=0; i<elements.length; i++){
+      //RECURSION 
+        arr.push(Number(elements[i]));
     }
+    
+    return arr;    
   }
+
+  
+   //number
+   function parseNumber(num){
+    //if we find . it's a digit
+      if (json.includes()) 
+      //if we find E notation it's scientific
+    //if we find E notation it's scientific
+
+   }
+
+   function parseString(string){
+    return string.substring(1,string.length-1);
+   }
+
+
+   function findType(json){
+
+     if ( json[0]==="[" ) {
+       parseArray(json);
+     }
+
+     if (typeof json[0] === "number" || json[0] === "-"){
+       parseNumber(json);
+     }
+
+     //string
+     if (json[0]==='"'){
+        parseString(json);
+     }
+
+     // null
+      if (json.includes("null")){
+        return null;
+      }
+
+     // boolean
+     if (json.includes("true")) {
+        return true;
+     } else if (json.includes("false")) {
+        return false;
+     }
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Invocation
+  parseJSON(json);
+
 };
+
+ 
